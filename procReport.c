0733d5bee365808d1e6d6c5c5543a72d607a935f
RomaBureacov <RomanBureacov@gmail.com>
2026-06-08 03:16:40 -0700

    added extra credit functionality

--- procReport.c
+++ procReport.c
@@ -51,28 +51,55 @@ int virt2phys(pgd_t* offset, unsigned long vpage) {
 void walker(void) {
     struct vm_area_struct* vma = 0;
     unsigned long vpage;
-    int numPages;
-    int totalPages = 0;
+    int numPages, non_contigpages, contigpages;
+    int totalPages = 0, totalContigPages = 0, totalNonContigPages = 0;
     struct task_struct* task;
     
-    printk(KERN_INFO "pid,name,total_pages\n");
+    printk(KERN_INFO "pid,name,contigpages,non_contigpages,total_pages\n");
     
     for_each_process(task) {
-    	numPages = 0;
+        unsigned long last_physpage = 0; 
+        numPages = contigpages = non_contigpages = 0;
+
         if (task_pid_nr(task) > 650 && task->mm && task->mm->mmap) {
             for (vma = task->mm->mmap; vma; vma = vma->vm_next) {
                 for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE) {
                     unsigned long physical_page_addr = virt2phys(pgd_offset(task->mm, vpage), vpage);
-                    if (physical_page_addr != 0) {
-                    	numPages++;
+
+                    // do we know this address is for this process?
+                    if (physical_page_addr != 0) { // this will always set the last physical page to something other than 0
+                        // could be the first page we see
+                        if (last_physpage != 0) {
+                            // this new address is one of two possibilities
+                            if (last_physpage + PAGE_SIZE != physical_page_addr) {
+                                non_contigpages++;
+                                totalNonContigPages++;
+                            } else {
+                                contigpages++;
+                                totalContigPages++;
+                            }
+
+                            if (numPages == 1) { // edge case, is the first page contig or not?
+                                if (totalNonContigPages == 1) { // if we found one non-contig, first one must be non-contig
+                                    non_contigpages++;
+                                    totalNonContigPages++;
+                                } else { // same reason for contig...
+                                    contigpages++;
+                                    totalContigPages++;
+                                }
+                            }
+                        }
+
+                        numPages++;
+                        last_physpage = physical_page_addr;
                     }
                 }
             }
-            printk(KERN_INFO "%d,%s,%d\n", task_pid_nr(task), task->comm, numPages);
-        	totalPages += numPages;
+            printk(KERN_INFO "%d,%s,%d,%d,%d\n", task_pid_nr(task), task->comm, contigpages, non_contigpages, numPages);
+            totalPages += numPages;
         }
     }
-    printk(KERN_INFO "Total pages: %d\n", totalPages);
+    printk(KERN_INFO "TOTALS,,%d,%d,%d\n", totalContigPages, totalNonContigPages, totalPages);
 }
 
 int proc_init(void) {
